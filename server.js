23d9e15251ed2e6b0e98a1efa3e002059edf7d3a
Georgi Mirchev <gmirchev90@gmail.com>
2021-01-05 19:03:56 +0200

    Fix port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const path = require('path');
 let app = express();
 app.use(express.static(path.join(__dirname, 'build')));
-const port = process.env.PORT || '8081';
+const port = process.env.PORT || 8081;
 app.set('port', port);
 app.use(express.static('build'));
 app.get('*', (req, res) => {
